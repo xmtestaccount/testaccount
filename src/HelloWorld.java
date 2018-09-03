652899ecd9e50d6d4589a829af299816d3fc9ef9
xmmxgit <1225151382@qq.com>
2018-09-03 10:14:17 +0800

    18.9.3

---
+++ src/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ * @program: JavaDemo
+ * @Date: 2018/8/28 10:21
+ * @Author: XuMian
+ * @Description:
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello Idea");
+        for (int i = 0; i < 100; i++) {
+            System.out.println(666);
+        }
+    }
+}
